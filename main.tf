b446f86b1415e8c2fbebfc1b230c61a52ef34bb4
japersa <japersa92@gmail.com>
2025-07-09 19:08:38 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.79"
+    }
+  }
+
+  required_version = ">= 1.4.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vm-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "vm-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes       = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig"
+    subnet_id                        = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "vmaztro"
+  computer_name         = "vmaztro"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = var.location
+  size                  = "Standard_B1s"
+  admin_username        = "admin_aztro"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "admin_aztro"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher   = "Canonical"
+    offer       = "0001-com-ubuntu-server-jammy"
+    sku         = "22_04-lts"
+    version     = "latest"
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+    description = "Nombre del resource group"
+    type        = string
+}
+
+variable "location" {
+    description = "Ubicacion del resource group"
+    type        = string
+}
